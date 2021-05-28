bd31ab176b84918ec976aaef51e4f70359a05215
HieuNguyen-1074 <hjeu1074@gmail.com>
2021-05-28 08:16:33 +0700

    Updates

---
+++ precache-manifest.fbc95e4361807b9bc16447abbe2b042d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66825268914d55462ffb40b1a1fc329d",
+    "url": "/cl-netflix/index.html"
+  },
+  {
+    "revision": "a2e1bda19d359728fbf0",
+    "url": "/cl-netflix/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "e6105a122cc1dda1c6d2",
+    "url": "/cl-netflix/static/css/main.1f8dab71.chunk.css"
+  },
+  {
+    "revision": "a2e1bda19d359728fbf0",
+    "url": "/cl-netflix/static/js/2.aca36253.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cl-netflix/static/js/2.aca36253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6105a122cc1dda1c6d2",
+    "url": "/cl-netflix/static/js/main.366d84dc.chunk.js"
+  },
+  {
+    "revision": "eb4f987027b122ea50f6",
+    "url": "/cl-netflix/static/js/runtime-main.65f8b60d.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/cl-netflix/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/cl-netflix/static/media/logo.35f3a5ae.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/cl-netflix/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/cl-netflix/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/cl-netflix/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/cl-netflix/static/media/tab-tv.9f91203b.png"
+  }
+]);
